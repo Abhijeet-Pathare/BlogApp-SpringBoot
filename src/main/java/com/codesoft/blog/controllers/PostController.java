b55fa985694047130e985673e6ea084aa8749eea
Abhijeet Pathare <abhijeetpathare2020@gmail.com>
2024-01-22 13:15:32 +0530

    added image download functionality

--- src/main/java/com/codesoft/blog/controllers/PostController.java
+++ src/main/java/com/codesoft/blog/controllers/PostController.java
@@ -7,15 +7,19 @@ import com.codesoft.blog.payloads.PostDto;
 import com.codesoft.blog.payloads.PostResponse;
 import com.codesoft.blog.services.FileService;
 import com.codesoft.blog.services.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import static com.codesoft.blog.config.AppConstants.PAGE_SIZE;
@@ -116,5 +120,15 @@ public class PostController {
         postDto.setImageName(fileName);
         PostDto updatedPost = postService.updatePost(postDto,postId);
         return new ResponseEntity<>(updatedPost,HttpStatus.OK);
+    }
+
+
+    //method to serve files
+    @GetMapping(value = "/profiles/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(@PathVariable("imageName") String imageName,
+                              HttpServletResponse response) throws IOException{
+        InputStream resource = fileService.getResource(path,imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource,response.getOutputStream());
     }
 }
